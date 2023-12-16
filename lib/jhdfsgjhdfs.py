83ed6515a3f730762aec2b30402c4419a591a042
Musicalibree <ernestodcruz05@gmail.com>
2023-12-16 10:54:47 +0800

    second commit

--- lib/jhdfsgjhdfs.py
+++
@@ -1,9 +0,0 @@
-range = int(input("enter a range: "))
-num1 = int(input("enter a number: "))
-num2 = int(input("enter a number: "))
-num3 = int(input("enter a number: "))
-num4 = int(input("enter a number: "))
-num5 = int(input("enter a number: "))
-
-result = num1 + num2 + num3 + num4 + num5
-print(num1,num2,num3,num4,num5)
